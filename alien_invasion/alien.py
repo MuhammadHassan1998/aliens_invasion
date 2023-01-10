5fba00f261ea1576baa1df2127b547db55ac4eeb
MuhammadHassanDevsinc <hassan.amin@devsinc.com>
2023-01-09 16:12:21 -0800

    Aliens fleet

---
+++ alien_invasion/alien.py
@@ -0,0 +1,42 @@
+import logging
+
+import pygame
+from pygame.sprite import Sprite
+
+
+log = logging.getLogger("logger")
+log.setLevel(logging.ERROR)
+
+
+class Alien(Sprite):
+    """Player Ship that moves and throw fire"""
+
+    def __init__(self, screen, settings) -> None:
+        super().__init__()
+        # ship settings
+        self.screen = screen
+        # set the settings
+        self.settings = settings
+        # get the image
+        try:
+            self.image = pygame.image.load("alien_invasion/images/alien.bmp")
+        except FileNotFoundError:
+            log.error("Can not find the alien image")
+            raise FileNotFoundError
+        else:
+            self.rect = self.image.get_rect()
+
+            # start from the top left
+            self.rect.x = self.rect.width
+            self.rect.y = self.rect.height
+
+            # alien position
+            self.x = float(self.rect.x)
+
+    def blitme(self) -> None:
+        self.screen.blit(self.image, self.rect)
+
+    def update(self) -> None:
+        # Move the aliens
+        self.x += self.settings.alien_speed_factor
+        self.rect.x = self.x
--- alien_invasion/game_functions.py
+++ alien_invasion/game_functions.py
@@ -3,6 +3,7 @@ import sys
 import pygame
 
 from bullet import Bullet
+from alien import Alien
 
 
 def left_right_up_down_movement(ship, event, is_down=False) -> None:
@@ -41,18 +42,66 @@ def check_event(ship, screen, bullets, settings) -> None:
             left_right_up_down_movement(ship=ship, event=event)
 
 
-def update_screen(settings, screen, ship, bullets) -> None:
+def update_screen(settings, screen, ship, bullets, aliens) -> None:
     # updates the screen every time
     screen.fill(settings.bg_color)
     for bullet in bullets.sprites():
         bullet.draw_bullet()
+    aliens.draw(screen)
     ship.blitme()
     # Make the recent screen visible
     pygame.display.flip()
 
 
-def update_bullets(bullets):
+def update_bullets(bullets) -> None:
     # remove the unused bullets
     for bullet in bullets.copy():
         if bullet.rect.bottom <= 0:
             bullets.remove(bullet)
+
+
+def get_alien_number(settings, alien_width) -> int:
+    # Get the number of aliens in a row
+    available_space_x = settings.screen_width - 2 * alien_width
+    number_aliens_x = int(available_space_x / (2 * alien_width))
+    return number_aliens_x
+
+
+def get_number_rows(settings, ship_height, alien_height) -> int:
+    # return number of rows available for aliens
+    available_space_y = settings.screen_height - (3 * alien_height) - ship_height
+    number_of_rows = int(available_space_y / (2 * alien_height))
+    return number_of_rows
+
+
+def create_alien(settings, screen, aliens, alien_number, row_number) -> None:
+    # Create row of aliens
+    alien = Alien(screen=screen, settings=settings)
+    alien_width = alien.rect.width
+    alien.x = alien_width + 2 * alien_width * alien_number
+    alien.rect.x = alien.x
+    alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
+    aliens.add(alien)
+
+
+def create_fleet(screen, settings, ship, aliens) -> None:
+    # creating fleet of aliens
+    alien = Alien(screen=screen, settings=settings)
+    alien_number_x = get_alien_number(settings=settings, alien_width=alien.rect.width)
+    rows = get_number_rows(
+        settings=settings, ship_height=ship.rect.height, alien_height=alien.rect.height
+    )
+    for row_number in range(rows):
+        for alien_number in range(alien_number_x):
+            create_alien(
+                settings=settings,
+                screen=screen,
+                aliens=aliens,
+                alien_number=alien_number,
+                row_number=row_number,
+            )
+
+
+def update_aliens(aliens) -> None:
+    # Update the position of aliens
+    aliens.update()
